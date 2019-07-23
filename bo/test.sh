eca23601f75de6df00b7d458962b6dcbe1582a0c
Xuan Zhang <xuanzhang@jhu.edu>
2019-07-23 14:50:20 -0400

    Append time to filename

--- bo/test.sh
+++ bo/test.sh
@@ -1,10 +1,11 @@
 source /export/a08/xzhan138/Auto-tuning/bayesian_optimization.config
 
 num_run=15
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 for dataset in 'ted-zh-en' 'ted-ru-en' 'robust19-en-ja' 'robust19-ja-en'
 do 
 	modeldir=/export/a10/kduh/p/mt/gridsearch/${dataset}/models/
-	output=${workdir}/testoutput/`date`_${dataset}_${architecture}_${rnn_cell_type}_${metric}_${embedding}_${embedding_distance}_${sampling_method}_${replacement}_${acquisition_func}_${model_type}_${kernel}_${weight}.res
+	output=${workdir}/testoutput/${current_time}_${dataset}_${architecture}_${rnn_cell_type}_${metric}_${embedding}_${embedding_distance}_${sampling_method}_${replacement}_${acquisition_func}_${model_type}_${kernel}_${weight}.res
 	python ${workdir}/bayesian_optimization.py --modeldir ${modeldir} \
 										   --architecture ${architecture} \
 										   --rnn-cell-type ${rnn_cell_type} \
--- gbssl/test.sh
+++ gbssl/test.sh
@@ -1,12 +1,13 @@
 source /export/a08/xzhan138/Auto-tuning/gbssl/gbssl.config
 
 num_run=15
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 for dataset in 'ted-zh-en' 'ted-ru-en' 'robust19-en-ja' 'robust19-ja-en'
 do
     modeldir=/export/a10/kduh/p/mt/gridsearch/${dataset}/models/
     for sparsity in 'full' 'knn'
     do
-        output=${workdir}/testoutput/`date`_${dataset}_${architecture}_${rnn_cell_type}_${model}_${metric}_${distance}_${distance_param}_${sparsity}_${k}.res
+        output=${workdir}/testoutput/${current_time}_${dataset}_${architecture}_${rnn_cell_type}_${model}_${metric}_${distance}_${distance_param}_${sparsity}_${k}.res
         python ${workdir}/gbssl.py --modeldir ${modeldir} \
                    --architecture ${architecture} \
                    --rnn-cell-type ${rnn_cell_type} \
